8113cfd96454bc5ffe00e1a5df65b8f96aa2d4d4
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2024-09-28 20:16:18 +0400

    K so I forgot to push this stuff from another file

--- core/parser.cpp
+++ core/parser.cpp
@@ -2,22 +2,33 @@
 
 bool Parser::not_EOF() const
 {
-
+    return !tokens.empty() && tokens[0].type != TokenType::_EOF;
 }
 
 Token Parser::at() const
 {
-    
+    return tokens.front();
 }
 
 Token Parser::eat()
 {
-
+    Token previous = tokens.front();
+        
+    tokens.erase(tokens.begin());
+        
+    return previous;
 }
 
 Token Parser::expect(TokenType type, std::string &err)
 {
-
+    Token previous = eat();
+    
+    if (previous.type != type) {
+        std::cerr << "Parser Error:\n" << err << " - Expecting: " << static_cast<int>(type) << std::endl;
+        
+        exit(1); 
+    }
+    return previous;
 }
 
 Program Parser::produceAST(std::string &sourceCode)
