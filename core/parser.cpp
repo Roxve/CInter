520d75f2be0880aba9bfcf947ee882db000f8b98
STierProgrammer <150156387+STierProgrammer@users.noreply.github.com>
2024-09-28 20:13:32 +0400

    Fixed some name errors

---
+++ core/parser.cpp
@@ -0,0 +1,26 @@
+#include "parser.hpp"
+
+bool Parser::not_EOF() const
+{
+
+}
+
+Token Parser::at() const
+{
+    
+}
+
+Token Parser::eat()
+{
+
+}
+
+Token Parser::expect(TokenType type, std::string &err)
+{
+
+}
+
+Program Parser::produceAST(std::string &sourceCode)
+{
+
+}
